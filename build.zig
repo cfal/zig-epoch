c92d526ddaf31adc4165ed73959804fa43437075
cfal <cfal@users.noreply.github.com>
2024-07-18 03:46:48 +0800

    build.zig: add addModule invocation

--- build.zig
+++ build.zig
@@ -15,6 +15,12 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("tempus", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "tempus",
         // In this case the main source file is merely a path, however, in more
